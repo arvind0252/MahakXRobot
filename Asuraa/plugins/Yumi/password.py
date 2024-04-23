ae58126f60b1c5e0b34edd8fe15f8d21b3a78c8f
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-04-23 15:31:51 +0530

    Update password.py

--- Asuraa/plugins/Yumi/password.py
+++ Asuraa/plugins/Yumi/password.py
@@ -15,7 +15,7 @@ async def password(bot, update):
         qw = random.choice(ST)
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
-    txt = f"๏ <b>ʟɪᴍɪᴛ ➠</b> {str(limit)} \n๏ <b>ᴘᴀꜱꜱᴡᴏʀᴅ ➠ <code>{random_value}</code>\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐"
+    txt = f"๏ <b>ʟɪᴍɪᴛ ➠</b> {str(limit)} \n๏ <b>ᴘᴀꜱꜱᴡᴏʀᴅ ➠ <code>{random_value}</code>\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙"
     btn = InlineKeyboardMarkup([[InlineKeyboardButton(' ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ', url='https://t.me/MahakXbot?startgroup=true')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
 
